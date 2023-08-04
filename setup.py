84c04176c869e868e105a1fe61833cc057ae7946
Xiao Gui <xgui3783@gmail.com>
2023-08-04 11:23:34 +0200

    debug setup

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ setup(
     packages=find_packages(include=["ebrains_util"]),
     python_requires=">=3.7",
     install_requires=[
-        "ebrains_iam @ git+https://github.com/xgui3783/ebrains-iam-util.git",
-        "ebrains_dataproxy_sync @ git+https://github.com/xgui3783/ebrains_dataproxy_sync.git",
+        "ebrains_iam",
+        "ebrains_dataproxy_sync",
         "ebrains-kg-snap",
     ],
     dependency_links=[
-        "git+https://github.com/xgui3783/ebrains-kg-snap.git#egg=ebrains-kg-snap"
+        "git+https://github.com/xgui3783/ebrains-kg-snap.git#egg=ebrains-kg-snap",
+        "git+https://github.com/xgui3783/ebrains_dataproxy_sync.git#egg=ebrains_dataproxy_sync",
+        "git+https://github.com/xgui3783/ebrains-iam-util.git#egg=ebrains_iam"
     ]
 )
