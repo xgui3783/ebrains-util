d23771ddb399bbda17cc989fa7d8440dc92072af
Xiao Gui <xgui3783@gmail.com>
2023-08-04 11:37:58 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ebrains-dataproxy-sync",
+    name="ebrains-util",
     version="0.0.1",
     author="Xiao Gui",
     author_email="xgui3783@gmail.com",
